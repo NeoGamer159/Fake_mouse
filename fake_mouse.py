7255dc45a04988dd1e43acfbdd3ae9aeb8a29178
Míla Černý <cernymiloslav98@gmail.com>
2025-12-17 21:02:23 +0100

    feat: initial commit - Pico fake mouse circuitpython

---
+++ fake_mouse.py
@@ -0,0 +1,25 @@
+import time
+import usb_hid
+import board
+from adafruit_hid.mouse import Mouse
+
+mouse = Mouse(usb_hid.devices)
+
+INTERVAL_MS = 60000
+STEP_DELAY_MS = 300     
+
+last_cycle = time.monotonic_ns() // 1_000_000
+state = 0  # 0 = čekání, 1 = návrat
+
+while True:
+    now = time.monotonic_ns() // 1_000_000
+
+    if state == 0 and (now - last_cycle) >= INTERVAL_MS:
+        mouse.move(x=1, y=0)
+        state = 1
+        step_time = now
+
+    elif state == 1 and (now - step_time) >= STEP_DELAY_MS:
+        mouse.move(x=-1, y=0)
+        last_cycle = now   
+        state = 0
